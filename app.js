2b9e47ff6b267c6e0a3d65306336c9a9a07388f1
Rohan-Netrakar <rohannetrakar25@gmail.com>
2025-09-18 15:06:38 +0530

    used array to store the issues and worked in /report router

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+import {Pool} from "pg";
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
+// PostgreSQL connection setup using environment variables
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
 // Fix __dirname with ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+// Check DB connection at startup
+pool.connect()
+    .then(client => {
+        console.log('Connected to PostgreSQL database.');
+        client.release(); // release the client back to the pool
+    })
+    .catch(err => {
+        console.error('Error connecting to PostgreSQL', err);
+        process.exit(1); // stop the app if DB connection fails
+    });
 
 // Set EJS as the template engine
 app.set("view engine", "ejs");
@@ -17,21 +45,30 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
+
+const issues = [];
+
 // Routes
 app.get("/", (req, res) => {
   res.render("index", {
     title: "My EJS App",
-    message: "Welcome to EJS Starter!",
+    issues: issues,
   });
 });
 
 //location recever router
-app.post("/location", (req, res) => {
-  const { username, latitude, longitude } = req.body;
+app.post("/report", (req, res) => {
+  const { username,category,description, latitude, longitude } = req.body;
   console.log("User's Location:", latitude, longitude);
-
+  issues.push({
+    username,
+    category,
+    description,
+    latitude,
+    longitude
+  })
   console.log(
-    `User's Data => Name: ${username}, Latitude: ${latitude}, Longitude: ${longitude}`
+    `User's Data => Name: ${username},category: ${category},description: ${description}, Latitude: ${latitude}, Longitude: ${longitude}`
   );
 
   res.send(
